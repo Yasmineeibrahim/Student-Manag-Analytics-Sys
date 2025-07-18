c18ea6deb8b9e7d7530d45a2098e7f593b61b52f
yasmine <yasminee455@gmail.com>
2025-07-18 14:25:22 +0400

    working STUDENT AND TEACHER LOGIN FORMS

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import gradeRouter from './routes/gradeRoute.js';
@@ -7,16 +6,17 @@ import router from './routes/studentRoute.js';
 import teacherRouter from './routes/teacherRoute.js';
 import courseRouter from './routes/courseRoute.js';
 import path from 'path';
-import bcrypt from 'bcrypt';
 import { fileURLToPath } from 'url';
 import Teacher from './models/teacherModel.js';
+import Student from './models/studentModel.js';
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 dotenv.config();
 const PORT = process.env.PORT || 9000;
 const MONGOURL = process.env.MONGO_URL;
@@ -62,6 +62,35 @@ app.post('/api/teacherLogin', async (req, res) => {
   }
 });
 
+
+app.post('/api/studentLogin', async (req, res) => {
+  console.log('BODY RECEIVED:', req.body);
+  let { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+  email = email.trim().toLowerCase();
+  console.log('Login attempt with email:', email);
+  try {
+    const student = await Student.findOne({ Email: email });
+    if (!student) {
+      console.log('Student not found for email:', email);
+      return res.status(404).json({ message: 'Student not found' });
+    }
+
+    if (password !== student.Password) {
+  return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const { Password, ...studentData } = student.toObject();
+
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
 app.use((req, res, next) => {
   console.log('REQUEST:', req.method, req.url, req.headers['content-type']);
   next();
@@ -71,5 +100,3 @@ app.use('/api/students', router);
 app.use('/api/courses', courseRouter);
 app.use('/api/grades', gradeRouter);
 app.use('/api/teachers',teacherRouter);
-
-
